c8a9313541bf7dd195be3044d50d9f992f47ec5c
mororz <ndrew.moroz@gmail.com>
2019-01-12 13:07:59 +0200

    Minor changes

--- init.el
+++ init.el
@@ -9,19 +9,3 @@
   (package-install 'use-package))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files nil)
- '(org-directory "~/org")
- '(package-selected-packages
-   (quote
-    (counsel-projectile projectile expand-region beacon yasnippet-snippets elpy zenburn-theme which-key use-package try solarized-theme smartparens org-bullets nyan-mode monokai-theme molokai-theme moe-theme material-theme counsel auto-complete ace-window))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
