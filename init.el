187c13566e88abea54aa1f8af8e2a59e85984182
mororz <ndrew.moroz@gmail.com>
2019-02-26 20:04:39 +0200

    Keeping up to date

--- init.el
+++ init.el
@@ -17,7 +17,7 @@
  '(org-directory "~/org")
  '(package-selected-packages
    (quote
-    (vimish-fold ess afternoon base16-theme ocean ocean-theme pdf-tools markdown-mode ein rainbow-mode ac-geiser rainbow-delimiters parinfer page-break-lines pcre2el dired+ zenburn-theme yasnippet-snippets wttrin which-key use-package undo-tree try telephone-line spacemacs-theme solarized-theme smartparens ranger py-autopep8 project-explorer powerline poet-theme parrot org-bullets org-ac nyan-mode multi-term moody monokai-theme molokai-theme moe-theme mode-icons minions material-theme magit leuven-theme image-dired+ htmlize heaven-and-hell expand-region elpy doom-modeline counsel-projectile beacon all-the-icons-dired afternoon-theme ace-window))))
+    (slime vimish-fold ess afternoon base16-theme ocean ocean-theme pdf-tools markdown-mode ein rainbow-mode ac-geiser rainbow-delimiters parinfer page-break-lines pcre2el dired+ zenburn-theme yasnippet-snippets wttrin which-key use-package undo-tree try telephone-line spacemacs-theme solarized-theme smartparens ranger py-autopep8 project-explorer powerline poet-theme parrot org-bullets org-ac nyan-mode multi-term moody monokai-theme molokai-theme moe-theme mode-icons minions material-theme magit leuven-theme image-dired+ htmlize heaven-and-hell expand-region elpy doom-modeline counsel-projectile beacon all-the-icons-dired afternoon-theme ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
