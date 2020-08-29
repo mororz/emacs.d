05f20bcd01ae72bcb84885582517d53b480ccebc
Andrew Moroz <ndrew.moroz@gmail.com>
2020-08-29 22:52:43 +0300

    Add scripts

---
+++ init.el
@@ -0,0 +1,16 @@
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (x86-lookup multi-term yasnippet magit org-bullets company-quickhelp company company-mode paredit smartparens which-key ace-window expand-region undo-tree rainbow-delimiters all-the-icons-dired beacon doom-modeline color-theme-sanityinc-tomorrow material-theme afternoon-theme use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+(put 'dired-find-alternate-file 'disabled nil)
